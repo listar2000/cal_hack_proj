77bc3ed8c700a67b09d2abfe86ad44a8bcca877b
李思达 <listar@airbears2-10-142-147-49.airbears2.1918.berkeley.edu>
2018-11-03 13:21:53 -0700

    add post logic

--- router.py
+++ router.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template,  redirect
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask('treeApp')
 
 @app.route('/', methods=['GET'])
-def hello():
+def index():
     return render_template('show_tree.html')
+
+@app.route('/search_tree', methods = ['POST'])
+def search_tree():
+    tree_search_id = request.form['tree_number']
+    print(tree_search_id)
+    return redirect(url_for('index'))
